d3398b0c261faf4af295668043b24ad6da0487ef
Roden Luo <roden.luo@gmail.com>
2024-09-30 19:56:51 +0300

    use all atoms; to be consistent with the paper

--- src/tool.py
+++ src/tool.py
@@ -1206,11 +1206,7 @@ class DiffFitTool(ToolInstance):
         from chimerax.map.molmap import molecule_map
         mol_vol = molecule_map(self.session, mol.atoms, self._single_fit_res.value(), grid_spacing=self.fit_vol.data.step[0])
 
-        backbone_atoms = ['N', 'CA', 'C', 'O']
-        is_backbone = np.isin(mol.atoms.names, backbone_atoms)
-
-        input_coords = mol.atoms.scene_coords[is_backbone]
-        # input_coords = mol.atoms.scene_coords
+        input_coords = mol.atoms.scene_coords
 
         # Fit
         timer_start = datetime.now()
