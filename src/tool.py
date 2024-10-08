0c653f0c77534b2e57663ead5417fc0795523bac
Roden Luo <roden.luo@gmail.com>
2024-10-08 10:51:45 +0300

    Utilities tab UI re-ordered

--- src/tool.py
+++ src/tool.py
@@ -690,21 +690,6 @@ class DiffFitTool(ToolInstance):
         layout.addWidget(doc_label, row, 0, 1, 3)
         row = row + 1
 
-        split_model_label = QLabel()
-        split_model_label.setText("Structure:")
-
-        from chimerax.map import Volume
-        from chimerax.atomic import Structure
-        from chimerax.ui.widgets import ModelMenuButton
-        self._split_model = sm = ModelMenuButton(self.session, class_filter=Structure)
-        mlist = self.session.models.list(type=Structure)
-        if mlist:
-            sm.value = mlist[0]
-
-        layout.addWidget(split_model_label, row, 0)
-        layout.addWidget(sm, row, 1, 1, 2)
-        row = row + 1
-
         split_out_dir_label = QLabel()
         split_out_dir_label.setText("Output Folder:")
         self.split_out_dir = QLineEdit()
@@ -717,27 +702,31 @@ class DiffFitTool(ToolInstance):
         layout.addWidget(split_out_dir_select, row, 2)
         row = row + 1
 
+        split_model_label = QLabel()
+        split_model_label.setText("Structure:")
+
+        from chimerax.map import Volume
+        from chimerax.atomic import Structure
+        from chimerax.ui.widgets import ModelMenuButton
+        self._split_model = sm = ModelMenuButton(self.session, class_filter=Structure)
+        mlist = self.session.models.list(type=Structure)
+        if mlist:
+            sm.value = mlist[0]
+
+        layout.addWidget(split_model_label, row, 0)
+        layout.addWidget(sm, row, 1)
+
         button = QPushButton()
         button.setText("Split")
         # button.clicked.connect(lambda: self.run_button_clicked())
-        layout.addWidget(button, row, 0, 1, 3)
+        layout.addWidget(button, row, 2)
+
         row = row + 1
+
 
         doc_label = QLabel("<b>Simulate a map for each structure in the folder</b>")
         doc_label.setWordWrap(True)
         layout.addWidget(doc_label, row, 0, 1, 3)
-        row = row + 1
-
-        sim_dir_label = QLabel()
-        sim_dir_label.setText("Structures Folder:")
-        self.sim_dir = QLineEdit()
-        self.sim_dir.textChanged.connect(lambda: self.store_settings())
-        sim_dir_select = QPushButton("Select")
-        sim_dir_select.clicked.connect(
-            lambda: self.select_clicked("Folder containing the structures", self.sim_dir))
-        layout.addWidget(sim_dir_label, row, 0)
-        layout.addWidget(self.sim_dir, row, 1)
-        layout.addWidget(sim_dir_select, row, 2)
         row = row + 1
 
         sim_out_dir_label = QLabel()
@@ -752,6 +741,18 @@ class DiffFitTool(ToolInstance):
         layout.addWidget(sim_out_dir_select, row, 2)
         row = row + 1
 
+        sim_dir_label = QLabel()
+        sim_dir_label.setText("Structures Folder:")
+        self.sim_dir = QLineEdit()
+        self.sim_dir.textChanged.connect(lambda: self.store_settings())
+        sim_dir_select = QPushButton("Select")
+        sim_dir_select.clicked.connect(
+            lambda: self.select_clicked("Folder containing the structures", self.sim_dir))
+        layout.addWidget(sim_dir_label, row, 0)
+        layout.addWidget(self.sim_dir, row, 1)
+        layout.addWidget(sim_dir_select, row, 2)
+        row = row + 1
+
         sim_resolution_label = QLabel()
         sim_resolution_label.setText("Resolution (Å):")
         self.sim_resolution = QDoubleSpinBox()
@@ -761,13 +762,12 @@ class DiffFitTool(ToolInstance):
         self.sim_resolution.setDecimals(4)
         self.sim_resolution.valueChanged.connect(lambda: self.store_settings())
         layout.addWidget(sim_resolution_label, row, 0)
-        layout.addWidget(self.sim_resolution, row, 1, 1, 2)
-        row = row + 1
+        layout.addWidget(self.sim_resolution, row, 1)
 
         button = QPushButton()
         button.setText("Simulate")
         # button.clicked.connect(lambda: self.run_button_clicked())
-        layout.addWidget(button, row, 0, 1, 3)
+        layout.addWidget(button, row, 2)
         row = row + 1
 
         vertical_spacer = QSpacerItem(1, 1, QSizePolicy.Minimum, QSizePolicy.Expanding)
