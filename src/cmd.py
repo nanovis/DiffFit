675ed885bde7ac7bc9c9d4b154bd8b0391155d13
Deng Luo <roden.luo@gmail.com>
2024-11-04 13:23:50 +0300

    dfit cmd desc and func def header

--- src/cmd.py
+++ src/cmd.py
@@ -1,45 +1,95 @@
 # vim: set expandtab shiftwidth=4 softtabstop=4:
 
 from chimerax.core.commands import CmdDesc      # Command description
-from chimerax.atomic import AtomsArg            # Collection of atoms argument
-from chimerax.core.commands import BoolArg      # Boolean argument
-from chimerax.core.commands import ColorArg     # Color argument
-from chimerax.core.commands import IntArg       # Integer argument
+from chimerax.atomic import StructureArg, AtomsArg            # Collection of atoms argument
+from chimerax.core.commands import BoolArg, ColorArg, IntArg, FloatArg, StringArg, SaveFolderNameArg
 from chimerax.core.commands import EmptyArg     # (see below)
 from chimerax.core.commands import Or, Bounded  # Argument modifiers
+from chimerax.map.mapargs import MapArg
 
+import os
+from datetime import datetime
 
 # ==========================================================================
 # Functions and descriptions for registering using ChimeraX bundle API
 # ==========================================================================
 
 
-def dfit(session, atoms, weighted=False, transformed=True):
-    """Report center of mass of given atoms."""
+def dfit(session, mol, in_map,
+         level=None,
+         sim_res=5.0,
+         num_positions=10,
+         num_rotations=100,
+         smooth_by="PyTorch iterative Gaussian",
+         smooth_loops=3,
+         kernel_sizes="[5, 5, 5]",
+         Gaussian_mode="Gaussian with negative (shrink)",
+         fit_atom_mode="Backbone",
+         out_dir="DiffFit_out/interactive",
+         device=None):
+    """Fit a single structure into a volume map"""
 
-    # ``session``     - ``chimerax.core.session.Session`` instance
-    # ``atoms``       - ``chimerax.atomic.Atoms`` instance or None
-    # ``weighted``    - boolean, whether to include atomic mass in calculation
-    # ``transformed`` - boolean, use scene rather than original coordinates
+    _save_results = True
+    _out_dir_exist_ok = True
+    _out_dir = out_dir
 
-    atoms, coords, cofm = _get_cofm(session, atoms, transformed, weighted)
-    session.logger.info("%s center of mass: %s" %
-                        ("weighted" if weighted else "unweighted", cofm))
+    use_level = in_map.maximum_surface_level
+    if level is not None:
+        use_level = level
+
+    if _save_results:
+
+        os.makedirs(_out_dir, exist_ok=_out_dir_exist_ok)
+        with open(f"{_out_dir}/log.log", "a") as log_file:
+            log_file.write(f"=======\n"
+                           f"Wall clock time: {datetime.now()}\n"
+                           f"-------\n"
+                           f"Interactive mode\n"
+                           f"Target Volume: {in_map.path}\n"
+                           f"Structure: {mol.filename}\n"
+                           f"Target Surface Threshold: {use_level}\n"
+                           f"-------\n"
+                           f"Sim-map resolution: {sim_res}\n"
+                           f"# positions: {num_positions}\n"
+                           f"# rotations: {num_rotations}\n"
+                           f"Smooth by: \"{smooth_by}\"\n"
+                           f"Smooth loops: {smooth_loops}\n"
+                           f"Kernel sizes: \"{kernel_sizes}\"\n"
+                           f"Gaussian mode: \"{Gaussian_mode}\"\n"
+                           f"Fit atom mode: \"{fit_atom_mode}\"\n"
+                           f"-------\n")
 
 
-dfit_desc = CmdDesc(required=[("atoms", Or(AtomsArg, EmptyArg))],
-                    optional=[("weighted", BoolArg),
-                              ("transformed", BoolArg)])
+dfit_desc = CmdDesc(required=[("mol", StructureArg)],
+                    keyword=[("in_map", MapArg),
+                             ("level", FloatArg),
+                             ("sim_res", FloatArg),
+                             ("num_positions", IntArg),
+                             ("num_rotations", IntArg),
+                             ("smooth_by", StringArg),
+                             ("smooth_loops", StringArg),
+                             ("kernel_sizes", StringArg),
+                             ("Gaussian_mode", StringArg),
+                             ("fit_atom_mode", StringArg),
+                             ("out_dir", SaveFolderNameArg),
+                             ("device", StringArg)],
+                    required_arguments=['in_map'])
 
 # Example commands
 # dfit #1 in #2
-# dfit #1 in #2 level 0.7 sim_res 5.0 num_p 10 num_r 100 smooth_by smooth_loops kernel_sizes gaussian_mode fit_atom_mode out_dir device
+# dfit #1 in #2
+#      level 0.7 sim_res 5.0
+#      num_p 10 num_r 100
+#      smooth_by smooth_loops kernel_sizes gaussian_mode
+#      fit_atom_mode out_dir device
 #
-# dfit multi str_dir sim_dir in map level 0.7 num_p 10 num_r 100
-# smooth_by smooth_loops smooth_weights kernel_sizes gaussian_mode fit_atom_mode out_dir device
-# negative_space
-# learning_rate
-# n_iters
+# dfit multi str_dir sim_dir in map
+#      level 0.7 num_p 10 num_r 100
+#      smooth_by smooth_loops smooth_weights kernel_sizes gaussian_mode
+#      fit_atom_mode out_dir device
+#      negative_space
+#      learning_rate
+#      n_iters
 
 
 
