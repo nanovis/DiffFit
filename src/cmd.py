52d1bb9d3db8c310ce56a5e87de10fcafb0dd80a
Deng Luo <roden.luo@gmail.com>
2024-11-05 12:06:26 +0300

    dfit disk cmd header

--- src/cmd.py
+++ src/cmd.py
@@ -2,7 +2,9 @@
 
 from chimerax.core.commands import CmdDesc      # Command description
 from chimerax.atomic import StructureArg, AtomsArg            # Collection of atoms argument
-from chimerax.core.commands import BoolArg, ColorArg, IntArg, FloatArg, StringArg, SaveFolderNameArg
+from chimerax.core.commands import (BoolArg, ColorArg, IntArg, FloatArg, StringArg,
+                                    SaveFolderNameArg, OpenFileNameArg, SaveFileNameArg,
+                                    OpenFolderNameArg, SaveFolderNameArg)
 from chimerax.core.commands import EmptyArg     # (see below)
 from chimerax.core.commands import Or, Bounded  # Argument modifiers
 from chimerax.map.mapargs import MapArg
@@ -195,7 +197,7 @@ dfit_desc = CmdDesc(required=[("mol", StructureArg)],
                              ("fit_atom_mode", StringArg),
                              ("out_dir", SaveFolderNameArg),
                              ("device", StringArg)],
-                    required_arguments=['in_map'])
+                    required_arguments=["in_map"])
 
 # Example commands
 # dfit #1 in #2
@@ -204,9 +206,57 @@ dfit_desc = CmdDesc(required=[("mol", StructureArg)],
 #      num_p 10 num_r 100
 #      smooth_by smooth_loops kernel_sizes gaussian_mode
 #      fit_atom_mode out_dir device
-#
-# dfit multi str_dir sim_dir in map
-#      level 0.7 num_p 10 num_r 100
+
+
+def dfit_disk(session, str_dir, sim_dir, in_map, level,
+
+              num_positions=10,
+              num_rotations=100,
+
+              smooth_loops=3,
+              kernel_sizes="[5, 5, 5]",
+              smooth_weights="[1.0, 1.0, 1.0]",
+
+              Gaussian_mode="Gaussian with negative (shrink)",
+              fit_atom_mode="Backbone",
+              negative_space=-0.5,
+
+              learning_rate=0.01,
+              n_iters=201,
+
+              out_dir="DiffFit_out/disk",
+              device=None):
+    """Fit a list of structures from disk into a volume map"""
+
+
+dfit_disk_desc = CmdDesc(keyword=[("str_dir", OpenFolderNameArg),
+                                  ("sim_dir", OpenFolderNameArg),
+                                  ("in_map", OpenFileNameArg),
+                                  ("level", FloatArg),
+
+                                  ("num_positions", IntArg),
+                                  ("num_rotations", IntArg),
+
+                                  ("smooth_loops", StringArg),
+                                  ("kernel_sizes", StringArg),
+                                  ("smooth_weights", StringArg),
+
+                                  ("Gaussian_mode", StringArg),
+                                  ("fit_atom_mode", StringArg),
+                                  ("negative_space", FloatArg),
+
+                                  ("learning_rate", FloatArg),
+                                  ("n_iters", IntArg),
+
+                                  ("out_dir", SaveFolderNameArg),
+                                  ("device", StringArg)],
+                         required_arguments=["str_dir",
+                                             "sim_dir",
+                                             "in_map",
+                                             "level"])
+
+# dfit disk str_dir dir sim_dir dir in map level 0.7
+#      num_p 10 num_r 100
 #      smooth_by smooth_loops smooth_weights kernel_sizes gaussian_mode
 #      fit_atom_mode out_dir device
 #      negative_space
@@ -214,84 +264,9 @@ dfit_desc = CmdDesc(required=[("mol", StructureArg)],
 #      n_iters
 
 
-
-def dfit_disk(session, atoms, color, weighted=False, transformed=True, count=1):
-    """Highlight the atoms nearest the center of mass of given atoms."""
-
-    # ``session``     - ``chimerax.core.session.Session`` instance
-    # ``atoms``       - ``chimerax.atomic.Atoms`` instance or None
-    # ``color``       - ``chimerax.core.colors.Color` instance
-    # ``weighted``    - boolean, whether to include atomic mass in calculation
-    # ``transformed`` - boolean, use scene rather than original coordinates
-
-    # Compute the center of mass first
-    atoms, coords, cofm = _get_cofm(session, atoms, transformed, weighted)
-
-    # Compute the distance of each atom from the cofm
-    # using the NumPy vector norm function
-    from numpy.linalg import norm
-    distances = norm(coords - cofm, axis=1)
-
-    # Sort the array and get the "count" indices to the closest atoms
-    if count > len(atoms):
-        count = len(atoms)
-    from numpy import argsort
-    atom_indices = argsort(distances)[:count]
-
-    # Create a collection of atoms from the indices
-    chosen = atoms[atom_indices]
-
-    # Update their "colors".  Assigning a single value to an
-    # array means assign the same value for all elements.
-    chosen.colors = color.uint8x4()
-
-
-dfit_disk_desc = CmdDesc(required=[("atoms", Or(AtomsArg, EmptyArg)),
-                                   ("color", ColorArg)],
-                         optional=[("weighted", BoolArg),
-                                   ("transformed", BoolArg),
-                                   ("count", Bounded(IntArg, 1, 50))])
-
-
 # ==========================================================================
 # Functions intended only for internal use by bundle
 # ==========================================================================
-
-
-def _get_cofm(session, atoms, transformed, weighted):
-    # ``session``     - ``chimerax.core.session.Session`` instance
-    # ``atoms``       - ``chimerax.atomic.Atoms`` instance
-    # ``transformed`` - boolean, use scene rather than original coordinates
-    # ``weighted``    - boolean, whether to include atomic mass in calculation
-
-    # If user did not specify the list of atoms, use all atoms
-    if atoms is None:
-        from chimerax.core.commands import all_objects
-        atoms = all_objects(session).atoms
-
-    # We can use either transformed or untransformed coordinates.
-    # Transformed coordinates are "scene coordinates", which
-    # takes into account translation and rotation of individual
-    # models.  Untransformed coordinates are the coordinates
-    # read from the data files.
-    if transformed:
-        coords = atoms.scene_coords
-    else:
-        coords = atoms.coords
-
-    # ``coords`` is a ``numpy`` float array of shape (N, 3)
-    # If we want weighted center, we have to multiply coordinates
-    # by the atomic mass
-    if not weighted:
-        cofm = coords.mean(axis=0)
-    else:
-        m = atoms.elements.masses
-        c = coords * m[:,None]
-        cofm = c.sum(axis=0) / m.sum()
-
-    # To get the average coordinates, we use ``numpy.mean``
-    # print("DEBUG: center of mass:", cofm)
-    return atoms, coords, cofm
 
 
 def _create_volume_conv_list(session, vol, smooth_by, smooth_loops, smooth_kernel_sizes, Gaussian_mode, device, negative_space_value=-0.5):
