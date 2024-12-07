38cc28384238b31565828f40ad00a016d129eb4f
Roden Luo <roden.luo@gmail.com>
2024-12-07 14:58:55 +0300

    call view command at the end of show results

--- src/tool.py
+++ src/tool.py
@@ -1546,6 +1546,8 @@ class DiffFitTool(ToolInstance):
         self.mol_paths = mol_paths
         self.cluster_idx = 0
 
+        run(self.session, "view")
+
     def _create_volume_conv_list(self, vol, smooth_by, smooth_loops, session, negative_space_value=-0.5):
         # From here on, there are three strategies for utilizing gaussian smooth
         # 1. with increasing sDev on the same input volume
